2b5667af9c3b1e72305cf63431a83d1ac51ada0b
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-10-29 03:35:15 +0300

    2

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -39,7 +39,7 @@ class Server:
         dataSet = self.dataset()
         return dataSet[tup[0]: tup[1]]
 
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Any]:
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Mapping[str, Any]:
         """Function"""
         data = self.get_page(page, page_size)
         dataSet = self.dataset()
