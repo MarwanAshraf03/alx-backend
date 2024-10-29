bcd78ff3e3f291946e4be5541cc4c3ac9f125051
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-10-29 03:20:50 +0300

    2

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -2,7 +2,7 @@
 """Module"""
 from typing import Tuple
 from typing import List
-from typing import Dict
+from typing import Mapping
 import csv
 import math
 
@@ -41,7 +41,7 @@ class Server:
         dataSet = self.dataset()
         return dataSet[tup[0]: tup[1]]
 
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Mapping:
         """Function"""
         data = self.get_page(page, page_size)
         dataSet = self.dataset()
