ba2dba5913ae3be42952c2cd1541e0fd8b6526b4
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-11-01 16:57:43 +0200

    2

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""basic cache class Module"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """BasicCache Class"""
+
+    def __init__(self):
+        """initialization"""
+        super().__init__()
+
+    def put(self, key, item):
+        """inputs data in the cache dictionary in FIFO order"""
+        if not (key and item):
+            return
+        klist = list(self.cache_data.keys())
+        if key in klist:
+            del self.cache_data[key]
+        if (key not in klist) and (len(klist) >= self.MAX_ITEMS):
+            print(f"DISCARD: {klist[-1]}")
+            del self.cache_data[klist[-1]]
+            self.cache_data[key] = item
+        else:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """gets data from the cache dictionary"""
+        if not key:
+            return None
+        if key not in self.cache_data.keys():
+            return None
+        return self.cache_data[key]
