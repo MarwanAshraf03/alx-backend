b77b37255277fda862f28bcbf84fabea8d20b78c
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-11-05 14:35:27 +0200

    0

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""simple module"""
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    """returns 0-index.html template"""
+    return render_template("0-index.html")
