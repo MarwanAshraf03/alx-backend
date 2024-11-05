71b98295516f50fae4f02ab977178d42e905d932
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-11-05 16:52:15 +0200

    3

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -26,7 +26,7 @@ app.config.from_object(Config)
 @app.route("/")
 def hello_world() -> str:
     """returns 2-index.html template"""
-    return render_template("2-index.html")
+    return render_template("3-index.html")
 
 
 if __name__ == "__main__":
