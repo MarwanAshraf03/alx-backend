1addf1ba0c673ddcd7d63a64c43a4e3cdc20c4a2
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-11-05 19:30:05 +0200

    3

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -2,15 +2,15 @@
 """simple module to render a web page"""
 from flask import Flask, render_template, request, session
 from typing import Union
-from flask_babel import Babel
+from flask_babel import Babel, gettext
 app = Flask(__name__)
-babel = Babel(app)
 
 
-@babel.localeselector
+# @babel.localeselector
 def get_locale() -> Union[str, None]:
     """determine the best match with our supported languages"""
-    return request.accept_languages.best_match(['en', 'es'])
+    # return request.accept_languages.best_match(['en', 'es'])
+    return "fr"
 
 
 class Config:
@@ -21,6 +21,7 @@ class Config:
 
 
 app.config.from_object(Config)
+babel = Babel(app, locale_selector=get_locale)
 
 
 @app.route("/")
