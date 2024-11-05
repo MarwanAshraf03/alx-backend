d90e2722dde4490417a3c0a766dfc8df371f95af
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-11-05 16:33:56 +0200

    2

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""simple module to render a web page"""
+from flask import Flask, render_template, request, session
+import flask_babel
+from flask_babel import Babel
+import babel
+app = Flask(__name__)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(['en', 'es'])
+
+
+class Config:
+    """a class to configure the babel instance"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@app.route("/")
+def hello_world() -> str:
+    """returns 0-index.html template"""
+    return render_template("1-index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
