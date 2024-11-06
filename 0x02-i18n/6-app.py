9051c22bc4cd329a01b4dcd87f6edda0b8eb92ea
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-11-06 02:17:26 +0200

    6

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -31,6 +31,7 @@ def get_locale():
     """determine the best match with our supported languages"""
     user_locale = get_user()
     header_locale = request.headers.get("locale")
+    print(header_locale)
     locale = request.args.get("locale")
     if locale in Config.LANGUAGES:
         return locale
@@ -38,6 +39,8 @@ def get_locale():
         return user_locale["locale"]
     elif header_locale:
         return header_locale
+    elif Config.BABEL_DEFAULT_LOCALE:
+        return Config.BABEL_DEFAULT_LOCALE
     return request.accept_languages.best_match(['en', 'fr'])
 
 
